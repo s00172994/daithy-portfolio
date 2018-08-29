1138114bae2955c27f806d6bcbb96a1c57a89854
David Brennan <david.brennan@mail.itsligo.ie>
2018-08-29 20:38:57 +0100

    Update MailChimp URL.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,7 @@
     
     var cfg = {
         scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        mailChimpURL   : 'https://daithy.us19.list-manage.com/subscribe/post?u=acaab7bdea92ded486a1929a6&amp;id=7de94776be'   // mailchimp url
     },
 
     $WIN = $(window);
